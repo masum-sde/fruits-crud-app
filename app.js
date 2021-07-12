8423f6f26181dcd5895b93364bf5d03ed0519fe2
Masum Khandaker <masum.sde@gmail.com>
2021-07-12 11:21:18 +0600

    api develop complete

--- app.js
+++ app.js
@@ -1,114 +1,157 @@
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
 const express = require('express');
 const bodyParser = require('body-parser');
-const db=require('./models');
-const {fruit}=require('./models');
-
+const cors = require('cors');
+const db = require('./models');
+const { fruit } = require('./models');
 const app = express();
-
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(bodyParser.json());
-
+app.use(cors());
 // bearer token validation function
-const isValidToken=(req)=>{
-    let token =req.headers.authorization;
-    if(token.startsWith('Bearer ')){
-        token=token.split(" ");
-        if(token[1]==="jibontabedona"){
+const isValidToken = (req) => {
+    let token = req.headers.authorization;
+    if (token.startsWith('Bearer ')) {
+        token = token.split(" ");
+        if (token[1] === "jibontabedona") {
             return true;
-        }else{
+        }
+        else {
             return false;
-            
         }
     }
-}
-
+};
 //add fruit data
-app.post('/addFruit',(req,res)=>{
-    if(isValidToken(req)){
-        fruit.create({
-            fruitName:req.body.fruitName,
-            fruitImageURL:req.body.fruitImageURL,
-            fruitVitamin:req.body.fruitVitamin,
-            fruitAvailability:req.body.fruitAvailability
-        }).then(result=>{
-            if (result) {
-                res.send(result);
-            } else {
-                res.status(400).send('Error in insert new record');
+app.post('/addFruit', (req, res) => {
+    if (isValidToken(req)) {
+        fruit.findOne({ where: { fruitName: req.body.fruitName } })
+            .then((result) => {
+            if (result === null) {
+                fruit.create({
+                    fruitName: req.body.fruitName,
+                    fruitImageURL: req.body.fruitImageURL,
+                    fruitVitamin: req.body.fruitVitamin,
+                    fruitAvailability: req.body.fruitAvailability
+                }).then((result) => {
+                    if (result) {
+                        res.send(result);
+                    }
+                    else {
+                        res.status(400).send('Error in insert new record');
+                    }
+                });
+            }
+            else {
+                res.send(req.body.fruitName + " is already exist.");
             }
         });
-
-    }else{
+    }
+    else {
         res.status(403).send("Unathorized Access");
-    }     
-    
+    }
 });
-
 //get all fruits
-app.get('/getAllFruits',(req,res)=>{    
-    if(isValidToken(req)){
+app.get('/getAllFruits', (req, res) => {
+    if (isValidToken(req)) {
         fruit.findAll()
-    .then(result=>{
-        res.send(result);
-    })
-    }else{
+            .then((result) => {
+            res.send(result);
+        });
+    }
+    else {
         res.status(403).send("Unathorized Access");
     }
-    
-})
-
+});
 //get single fruits
-app.get('/getSingleFruit/:id',(req,res)=>{    
-    if(isValidToken(req)){
-        fruit.findOne({ where: { id: req.params.id } })
-    .then(result=>{
-        res.send(result);
-    })
-    }else{
+app.get('/getSingleFruit/:id', (req, res) => {
+    if (isValidToken(req)) {
+        fruit.findOne({ where: req.params })
+            .then((result) => {
+            if (result) {
+                res.send(result);
+            }
+            else {
+                res.send("Not Found!!");
+            }
+        });
+    }
+    else {
         res.status(403).send("Unathorized Access");
     }
-    
-})
-
-//delete specific fruit
-app.delete('/deleteSingleFruit/:id',(req,res)=>{
-    if(isValidToken(req)){
-        fruit.destroy({where:{id:req.params.id}})
-        .then(data=>{
-            if(data>0){
+});
+//get single fruit by query example: http://localhost:5000/getSingleFruit?fruitName=apple
+app.get('/getSingleFruit', (req, res) => {
+    console.log(req.query);
+    if (isValidToken(req)) {
+        fruit.findOne({ where: req.query })
+            .then((result) => {
+            if (result) {
+                res.send(result);
+            }
+            else {
+                res.send("Not Found!!");
+            }
+        });
+    }
+    else {
+        res.status(403).send("Unathorized Access");
+    }
+});
+//get Single fruit by fruit name
+app.get('/getSingleFruitByName/:fruitName', (req, res) => {
+    console.log(req.params);
+    if (isValidToken(req)) {
+        fruit.findOne({ where: req.params })
+            .then((result) => {
+            if (result) {
+                res.send(result);
+            }
+            else {
+                res.send("Not Found!!");
+            }
+        });
+    }
+    else {
+        res.status(403).send("Unathorized Access");
+    }
+});
+//delete specific fruit by id
+app.delete('/deleteSingleFruit/:id', (req, res) => {
+    if (isValidToken(req)) {
+        fruit.destroy({ where: req.params })
+            .then((data) => {
+            if (data > 0) {
                 res.send("deleted successfull");
-            }else{
+            }
+            else {
                 res.send("delete operation failed");
             }
-        })
-    }else{
-        res.status(403).send("Unathorized Access");
-    }    
-})
-
-//udpate single fruit data
-app.patch('/updateSingleFruit/:id',(req,res)=>{
-    if(isValidToken(req)){
-        fruit.update(req.body,{where:{id:req.params.id}})
-        .then(result=>{
-            if(result[0]>0){
-                res.send("Update successfull")
-            }else{
-                res.send("Update failed");
-            }
-        })
-
-    }else{
+        });
+    }
+    else {
         res.status(403).send("Unathorized Access");
     }
-})
-
-
-
-
-db.sequelize.sync().then((req)=>{
-    app.listen(5000,()=>{
-        console.log("Listening From Server")
-    })
-})
-
+});
+//udpate single fruit data
+app.patch('/updateSingleFruit/:id', (req, res) => {
+    if (isValidToken(req)) {
+        fruit.update(req.body, { where: { id: req.params.id } })
+            .then((result) => {
+            if (result[0] > 0) {
+                res.send("Update successfull");
+            }
+            else {
+                res.send("Update failed");
+            }
+        });
+    }
+    else {
+        res.status(403).send("Unathorized Access");
+    }
+});
+db.sequelize.sync().then((req) => {
+    app.listen(process.env.PORT || 5000, () => {
+        console.log("Listening From Server");
+    });
+});
